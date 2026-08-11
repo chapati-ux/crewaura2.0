6e63c289c4a14f93d141e8ee555486d2ca976b49
jayesh pujari <jpujari575@gmail.com>
2026-08-12 03:28:59 +0530

    debug: log VITE_API_URL

--- Aura/src/pages/Contact.jsx
+++ Aura/src/pages/Contact.jsx
@@ -19,6 +19,7 @@ const CONTACT_API_URL = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/api/email`
   : '/api/email'
 
+  console.log("VITE_API_URL is:", import.meta.env.VITE_API_URL)
 // Google Maps iframe target embedding link
 const MAPS_EMBED_SRC =
   'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.6!2d72.8777!3d19.0760!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTnCsDA0JzMzLjYiTiA3MsKwNTInMzkuNyJF!5e0!3m2!1sen!2sin!4v1700000000000'
